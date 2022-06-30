db49e053f936a5b45aaade19cd12b5ee0efcb209
Jacob Debel <jacobdebel@gmail.com>
2022-06-30 18:27:52 +0200

    Initial commit

---
+++ Opgave_01.py
@@ -0,0 +1,7 @@
+# Dette er jeres foerste program
+# Alle linjer, der starter med # (hashtag) er kommentarer
+# og læses ikke af python-fortolkeren
+
+# Foelg vejledningen i README.org
+
+print("Hej med jer!")
